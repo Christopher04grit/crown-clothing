0dbf8373f5cb880ab7d2b5c391d5ab5bbb3d6d32
christopher04grit <chrizz11isok@gmail.com>
2023-07-16 13:20:10 +0100

    creating sign-in page, and sign-in components.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/hompage/homepage.component';
 
 import './App.css';
+
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 class App extends React.Component {
   render() {
@@ -16,6 +18,7 @@ class App extends React.Component {
         <Routes>
         <Route  path='/' element={<HomePage />}/>
         <Route  path='/shop' element={<ShopPage />}/>
+        <Route  path='/signIN' element={<SignInAndSignUp />}/>
         </Routes>
       </div>
     );
